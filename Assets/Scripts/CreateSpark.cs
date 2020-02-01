ebfc700e4ac5ab3d95012931bc56bea84c0baba4
Adam Heeren <thearpist123@gmail.com>
2020-02-01 12:59:16 -0500

    Adding first draft of sparks and asteroids

---
+++ Assets/Scripts/CreateSpark.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateSpark : MonoBehaviour
+{
+    public GameObject myspark;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myspark = Instantiate(myspark, new Vector2(0,0), Quaternion.identity) as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
